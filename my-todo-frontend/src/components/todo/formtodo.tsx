f5a346deebd36b50967497d3c566daef7d54ed99
kawji <24.supavich@gmail.com>
2025-10-25 22:26:12 +0700

    Update formtodo.tsx

--- my-todo-frontend/src/components/todo/formtodo.tsx
+++ my-todo-frontend/src/components/todo/formtodo.tsx
@@ -1,10 +1,23 @@
 "use client";
+import axios from "axios";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import clsx from "clsx";
 
 
+export interface TodoItem {
+  id: number;
+  content: string;
+  status: boolean;
+} 
+
+
 export default function FormTodo() {
-  const [task, setTask] = useState("");
+  const API_URL = "http://127.0.0.1:8000"; // FastAPI backend
+  const queryClient = useQueryClient();
+  const [task, setTask] = useState<string>("");
+
+
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -16,6 +29,29 @@ export default function FormTodo() {
 
   const total = task.length;
   const remit = 50 - total;
+
+
+  // ---------------- set fetch api ----------
+
+  const addTodo = useMutation({
+    mutationFn: async (newTask: string) => {
+      const res = await axios.post(`${API_URL}/todos`, {
+        content: newTask,
+        status: false,
+      });
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+      setTask("");
+    },
+  });
+
+  const handleAdd = () => {
+    if (task.trim()) {
+      addTodo.mutate(task);
+    }
+  };
 
   return (
     <div className="max-w-[550px] w-full flex flex-col justify-center items-center gap-4 py-8 border-b border-b-black/7 dark:border-b-white/10">
@@ -30,6 +66,7 @@ export default function FormTodo() {
         <button
           className="px-4 py-2 border dark:text-white/75 hover:bg-black/93 hover:text-white hover:dark:bg-white hover:dark:text-black hover:scale-105 border-black/10 dark:border-white/15 cursor-pointer transition-all duration-300"
           disabled={!task.trim()}
+          onClick={ handleAdd }
         >
           Add
         </button>
@@ -43,7 +80,7 @@ export default function FormTodo() {
         </span>
         <span
           className={clsx('px-2 py-1 border transition-all duration-300 ' 
-            , remit <= 5 ? "text-red-700 border-red-700 bg-red-500/25 hover:bg-red-500/5 " : "hover:bg-black/5 hover:dark:bg-white/5"
+            , remit <= 15 ? "text-red-700 border-red-700 bg-red-500/25 hover:bg-red-500/5 " : "hover:bg-black/5 hover:dark:bg-white/5"
           )}
         >
           Remaining: {remit}
