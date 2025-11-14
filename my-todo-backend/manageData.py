0d0951effa24ee695357fc69f9f7bf9dd208a74c
kawji <24.supavich@gmail.com>
2025-11-14 20:25:40 +0700

    Update manageData.py

--- my-todo-backend/manageData.py
+++ my-todo-backend/manageData.py
@@ -1,32 +1,27 @@
-import os
 from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base, sessionmaker
-
+from sqlalchemy.pool import NullPool
 # โหลดไฟล์ .env
 load_dotenv()
 
-# ดึงค่าจาก environment
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+# environment
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DBNAME = os.getenv("DBNAME")
 
-# สร้าง connection string สำหรับ PostgreSQL
-DATABASE_URL = f"postgresql://postgres:{DB_PASSWORD}@db.onhsywupgzlztfjsutnm.supabase.co:5432/postgres"
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
-Session = sessionmaker(bind=engine)
 
-# สร้างโมเดลตาราง
 class DatabaseModel(Base):
     __tablename__ = 'Data_todo'
     id = Column(Integer, primary_key=True, autoincrement=True)
     content = Column(String(50), nullable=False)
     status = Column(Boolean, nullable=False, default=False)
 
-# สร้างตาราง (ถ้ายังไม่มี)
-Base.metadata.create_all(engine)
-
+Base.metadata.create_all(bind=engine)
