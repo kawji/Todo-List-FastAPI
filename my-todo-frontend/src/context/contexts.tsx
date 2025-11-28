5b910cfc32fa4ecc0a5fea03a167c717dea1f5fc
kawji <24.supavich@gmail.com>
2025-11-28 20:43:36 +0700

    Create contexts.tsx

---
+++ my-todo-frontend/src/context/contexts.tsx
@@ -0,0 +1,12 @@
+
+import { createContext } from "react";
+import { CardDoProps } from "@/types/cardtodo";
+import { ContextType } from "@/types/contexttype";
+
+
+export const DataTodo = createContext<ContextType | null >(null);
+
+
+
+
+
