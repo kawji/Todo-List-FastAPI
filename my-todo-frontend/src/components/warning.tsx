ab2ae1ce2fd70b89186f0aa75ad0261ee11dfe63
カウ <24.supavich@gmail.com>
2026-06-01 12:58:02 +0700

    Implement WarningTag component with conditional rendering

--- my-todo-frontend/src/components/warning.tsx
+++ my-todo-frontend/src/components/warning.tsx
@@ -1,1 +1,29 @@
+"use client"
 
+import { useState } from "react"
+
+
+
+function WarningTag() {
+ const warning = "waiting for database supabase" || " "
+ const [checkWarning,setCheckWarning] = useState(false)
+  
+ return(
+  <div className="flex flex-col items-center justify-center  ">
+
+    {checkWarning && <button className="flex items-center justify-center border dark:border-white/75 dark:bg-white/15 dark:text-white/75 hover:dark:bg-white/90
+                       border-amber-50/75 bg-amber-50/15 text-black transition-all duration-300 cursor-normal "
+                       
+                       >
+    {warning}
+  </button>}
+    
+  </div>
+
+   )
+
+
+
+  
+  
+}
